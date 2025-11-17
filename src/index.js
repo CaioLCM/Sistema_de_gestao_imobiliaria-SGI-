3a761b2bb93ff3e1cffa7d1799ae6460b40c9cee
GuguTaets <d2024007029@unifei.edu.br>
2025-11-17 19:59:22 -0300

    refator/RFC01 a 04
    Melhorei a segurança.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Mantido do seu original
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; // Mantido do seu original
 import { BrowserRouter } from 'react-router-dom';
+
+// 1. ADICIONE ESTA LINHA
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* 2. ADICIONE O "AuthProvider" EM VOLTA DO "App" */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
